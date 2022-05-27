3c338f18a03e9cec8a2b89ed025c6d210f70db94
mauryanavi <navneet@sigaramtech.com>
2022-05-27 19:14:25 +0530

    Merge with AD

--- components/stepper/stepper.js
+++ components/stepper/stepper.js
@@ -13,7 +13,7 @@ function hexToRGB(hex, alpha) {
     }
 }
 
-const Stepper = ({ activeStep = 1, vertical = false, steps = [], sx = {},openReturn,details }) => {
+const Stepper = ({ activeStep = 1, vertical = false, steps = [], sx = {}, openReturn, details }) => {
     const style = {
         compoleted: {
             color: '#E83B3B'
@@ -82,18 +82,20 @@ const Stepper = ({ activeStep = 1, vertical = false, steps = [], sx = {},openRet
                     </div>
                 </div>
             </div >
-
-            <div className="mt-4 w-full border-t-2 border-[#00000] h-full">
-                <div className=" px-4 my-4  w-full flex justify-end  items-center  ">
-                    {
-                        details.orderStatus !== "CANCELLED_BY_CUSTOMER" ?
-                            <p className='btn-color-revers cursor-pointer mr-14 font-bold' onClick={() => openReturn(true)}>Cancel</p>
-                            :
-                            <p className='btn-color-revers cursor-pointer mr-14 font-bold'  >Order Cancelled</p>
-                    }
-                    {/* <p className='btn-color-revers cursor-pointer font-bold '>Need Help?</p> */}
+            {
+                details?.orderStatus &&
+                <div className="mt-4 w-full border-t-2 border-[#00000] h-full">
+                    <div className=" px-4 my-4  w-full flex justify-end  items-center  ">
+                        {
+                            details.orderStatus !== "CANCELLED_BY_CUSTOMER" ?
+                                <p className='btn-color-revers cursor-pointer mr-14 font-bold' onClick={() => openReturn(true)}>Cancel</p>
+                                :
+                                <p className='btn-color-revers cursor-pointer mr-14 font-bold'  >Order Cancelled</p>
+                        }
+                        {/* <p className='btn-color-revers cursor-pointer font-bold '>Need Help?</p> */}
+                    </div>
                 </div>
-            </div>
+            }
 
         </>
     )
