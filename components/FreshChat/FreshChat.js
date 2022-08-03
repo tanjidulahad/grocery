127167c2491858d4dae758acc28aa1d7a8910aca
itsmetanjid <ahad@sigaramtech.com>
2022-08-03 12:39:10 +0600

    zendesk and freshchat recode

--- components/FreshChat/FreshChat.js
+++ components/FreshChat/FreshChat.js
@@ -3,24 +3,36 @@ import React, { useState } from 'react';
 import { connect } from "react-redux";
 
 const FreshChat = ({widgets}) => {
-    const [freshChatCodekey, setFreshChatCodekey] = useState(null)
-    if (typeof window != 'undefined' && widgets != null) {
-        const parser = new DOMParser();
-        if (widgets?.FRESHCHAT?.record_status == "ACTIVE" && widgets?.FRESHCHAT?.integration_attributes?.code) {
-            const freshchatcode = parser.parseFromString(widgets?.FRESHCHAT?.integration_attributes?.code, "text/html")
-            const key = freshchatcode.head.getElementsByTagName('script')[0].src
-            if (!freshChatCodekey) {
-                setFreshChatCodekey(key)
-            }
-        }
-    }
+    // const [freshChatCodekey, setFreshChatCodekey] = useState(null)
+    // if (typeof window != 'undefined' && widgets != null) {
+    //     const parser = new DOMParser();
+    //     if (widgets?.FRESHCHAT?.record_status == "ACTIVE" && widgets?.FRESHCHAT?.integration_attributes?.code) {
+    //         const freshchatcode = parser.parseFromString(widgets?.FRESHCHAT?.integration_attributes?.code, "text/html")
+    //         const key = freshchatcode.head.getElementsByTagName('script')[0].src
+    //         if (!freshChatCodekey) {
+    //             setFreshChatCodekey(key)
+    //         }
+    //     }
+    // }
+
     return (
         <>
-            <Head>
-                {
-                    freshChatCodekey && <script src={`${freshChatCodekey}`} chat='true'> </script>
-                }
-            </Head>
+
+            {widgets != null ?
+                widgets?.FRESHCHAT ?
+                    widgets?.FRESHCHAT?.record_status == "ACTIVE" ?
+                        <>
+                            <Head>
+                            <script src={`//in.fw-cdn.com/${widgets?.FRESHCHAT?.integration_attributes?.key}.js`} chat='true'></script>
+                            </Head>
+                        </>
+                        :
+                        ""
+                    :
+                    ""
+                :
+                ""
+            }
         </>
     );
 };
--- components/ZendeskChat/ZendeskChat.js
+++ components/ZendeskChat/ZendeskChat.js
@@ -4,29 +4,27 @@ import { connect } from "react-redux";
 
 
 const ZendeskChat = ({ widgets }) => {
-    const [zendeskCodekey, setZendeskCodekey] = useState(null)
-    if (typeof window != 'undefined' && widgets != null) {
-        const parser = new DOMParser();
-        if (widgets?.ZENDESK?.record_status == "ACTIVE" && widgets?.ZENDESK?.integration_attributes?.code) {
-            const zendeskCode = parser.parseFromString(widgets?.ZENDESK?.integration_attributes?.code, "text/html")
-            const key = zendeskCode.head.getElementsByTagName('script')[0].src.split("key=")[1]
-            if (!zendeskCodekey && key) {
-                setZendeskCodekey(key)
-            }
-        }
-    }
-
+    
     return (
         <>
 
-            <Head>
-                {
-                    zendeskCodekey && <script id="ze-snippet" src={`https://static.zdassets.com/ekr/snippet.js?key=${zendeskCodekey}`}> </script>
-                }
-            </Head>
-
-
+            {widgets != null ?
+                widgets?.ZENDESK ?
+                    widgets?.ZENDESK?.record_status == "ACTIVE" ?
+                        <>
+                            <Head>
+                            <script id="ze-snippet" src={`https://static.zdassets.com/ekr/snippet.js?key=${widgets?.ZENDESK?.integration_attributes?.key}`}> </script>
+                            </Head>
+                        </>
+                        :
+                        ""
+                    :
+                    ""
+                :
+                ""
+            }
         </>
+
     );
 };
 
