676feabd86316ed11d8b75eeb9e1210abad6ca40
itsmetanjid <ahad@sigaramtech.com>
2022-08-24 14:34:10 +0600

    config change

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@
 const withPWA = require("next-pwa");
 const path = require('path')
 module.exports = withPWA({
-  // pwa: {
-  //   dest: "public",
-  //   register: true,
-  //   skipWaiting: true,
-  // },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
   
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
